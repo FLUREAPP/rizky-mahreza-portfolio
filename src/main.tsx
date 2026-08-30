cfea08d9f898418d43bc434d870daf4c5432fd4b
RIZKY MAHREZA <app.flure.dev@gmail.com>
2026-08-31 02:56:24 +0700

    feat: integrate shader background into portfolio

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import { ShaderBackground } from './components/ui/kk'
 import './styles.css'
+import './shader.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode><App /></StrictMode>,
+  <StrictMode>
+    <>
+      <div className="shader-page-background" aria-hidden="true">
+        <ShaderBackground className="h-full w-full" />
+      </div>
+      <App />
+    </>
+  </StrictMode>,
 )
